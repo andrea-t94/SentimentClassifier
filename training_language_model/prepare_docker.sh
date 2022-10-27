cde3c895ef714719737777285d776c42c85d964d
andreatamburri <andrea.tamburri@flixbus.com>
2022-10-27 16:11:13 +0200

    Refactor

--- training_language_model/prepare_docker.sh
+++ training_language_model/prepare_docker.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# it works for Linux AMI 2 on AWS
-
-# installing docker
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-apt-cache policy docker-ce
-sudo apt install docker-ce -Y
-
-# Configure Docker to start on boot
-sudo systemctl enable docker.service
-
-# Prepare Docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Make ec2-user to docker group to run command without sudo
-sudo usermod -aG docker ${USER}
+# dependencies
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+# add docker official GPG key
+sudo mkdir -p /etc/apt/keyrings
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg-
+# then add their repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+# finally install it
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# create the docker group
+sudo groupadd docker
+# add your user to the docker group
+sudo usermod -aG docker $USER
