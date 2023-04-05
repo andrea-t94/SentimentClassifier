6870fb191557b6853de6a1c0f71bf4a3b2790e05
andreatamburri <andrea.tamburri@flixbus.com>
2023-04-05 17:15:42 +0200

    Modify docker compose

--- training_language_model/fine_tuning_clf/main.py
+++ training_language_model/fine_tuning_clf/main.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import socket
 from datetime import datetime
 import os
+import warnings
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -24,6 +25,27 @@ from params.model import *
 
 
 def main():
+
+    print(f"torch version is {torch.__version__}")
+
+    if torch.cuda.is_available():
+        print(f"CUDA version {(torch.version.cuda)} is available with {torch.cuda.device_count()} devices")
+        if torch.backends.cudnn.version():
+            print(f"cuDNN version {torch.backends.cudnn.version()} is also available")
+        else:
+            print(f"no cuDNN")
+    try:
+        if not torch.backends.mps.is_available():
+            if not torch.backends.mps.is_built():
+                print("MPS not available because the current PyTorch install was not "
+                    "built with MPS enabled.")
+            else:
+                print("MPS not available because the current MacOS version is not 12.3+ "
+                    "and/or you do not have an MPS-enabled device on this machine.")
+        else:
+            print("MPS is available.")
+    except:
+        warnings.warn(f"current pytorch version {torch.__version__} doesn't support MPS backend")
     
     HF_USER = os.environ.get('HF_USER')
     HF_TOKEN = os.environ.get('HF_TOKEN')
